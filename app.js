73040ab123511af874bd94b3f5dd9043d8c58d14
Vivianaoso <vvivianaoso@gmail.com>
2022-07-15 14:22:52 -0500

    Mercado Liebre Bibi Osorio

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+// app.listen(3000, function(){
+//     console.log('Servidor funcionando');
+// });
+// app.listen(3000, function(){
+//     console.log('Servidor funcionando');
+// });
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Servidor funcionario');
+}) 
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
